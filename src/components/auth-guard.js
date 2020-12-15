364f98434872b7b08353fd28b4ffc38c042d26f1
juan-carlos-correa <juan.correa.herrera@gmail.com>
2020-12-15 13:43:30 -0600

    handle token on requets

--- src/components/auth-guard.js
+++ src/components/auth-guard.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import {AuthContext} from '../contexts/auth-context'
 import {decodeJwt} from '../utils/jwt-utils'
+import {setItem} from '../utils/storage-utils'
 
 export const AuthGuard = ({children}) => {
   const [isAuth, setIsAuth] = React.useState(false)
@@ -9,6 +10,7 @@ export const AuthGuard = ({children}) => {
 
   const onLoginSuccess = React.useCallback(({token}) => {
     const {username} = decodeJwt(token)
+    setItem({key: '@token', value: token})
     setIsAuth(true)
     setUser({username})
   }, [])
