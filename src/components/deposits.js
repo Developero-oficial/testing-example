32d6287b363fd563fc6312b3a400178864d8590e
juan-carlos-correa <juan.correa.herrera@gmail.com>
2020-12-15 08:29:49 -0600

    added dashboard content

---
+++ src/components/deposits.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {makeStyles} from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+
+const useStyles = makeStyles({
+  depositContext: {
+    flex: 1,
+  },
+})
+
+export const Deposits = () => {
+  const classes = useStyles()
+  return (
+    <React.Fragment>
+      <Typography component="h2" variant="h6" color="primary" gutterBottom>
+        Recent Deposits
+      </Typography>
+      <Typography component="p" variant="h4">
+        $3,024.00
+      </Typography>
+      <Typography color="textSecondary" className={classes.depositContext}>
+        on 15 March, 2019
+      </Typography>
+      <div>
+        <Typography>Updated</Typography>
+      </div>
+    </React.Fragment>
+  )
+}
--- src/pages/dashboard-page/dashboard-page.js
+++ src/pages/dashboard-page/dashboard-page.js
@@ -1,5 +1,106 @@
 import React from 'react'
+import {makeStyles} from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+
+import {Chart} from '../../components/chart'
+import {Deposits} from '../../components/deposits'
+import {Orders} from '../../components/orders'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+  toolbar: {
+    paddingRight: 24,
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  signoutButton: {
+    marginLeft: 36,
+  },
+  title: {
+    flexGrow: 1,
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    height: '100vh',
+    overflow: 'auto',
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+  },
+  fixedHeight: {
+    height: 240,
+  },
+}))
 
 export const DashboardPage = () => {
-  return <h1>DashboardPage</h1>
+  const classes = useStyles()
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="absolute" className={classes.appBar}>
+        <Toolbar className={classes.toolbar}>
+          <Typography
+            component="h1"
+            variant="h6"
+            color="inherit"
+            className={classes.title}
+          >
+            Dashboard
+          </Typography>
+          <Typography>John Doe</Typography>
+          <Button
+            color="secondary"
+            variant="contained"
+            className={classes.signoutButton}
+          >
+            Sign out
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <main className={classes.content}>
+        <div className={classes.appBarSpacer} />
+        <Container maxWidth="lg" className={classes.container}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={8} lg={9}>
+              <Paper className={`${classes.paper} ${classes.fixedHeight}`}>
+                <Chart />
+              </Paper>
+            </Grid>
+            <Grid item xs={12} md={4} lg={3}>
+              <Paper className={`${classes.paper} ${classes.fixedHeight}`}>
+                <Deposits />
+              </Paper>
+            </Grid>
+
+            <Grid item xs={12}>
+              <Paper className={`${classes.paper} ${classes.fixedHeight}`}>
+                <Orders />
+              </Paper>
+            </Grid>
+          </Grid>
+        </Container>
+      </main>
+    </div>
+  )
 }
